42c1e730f7e589772e24f3bb4d763ae6ac3413ba
Eqls <argnow12@gmail.com>
2024-03-17 20:11:40 +0100

    Initial commit.

---
+++ Makefile
@@ -0,0 +1,2 @@
+default:
+	clang -std=clang src/main.cpp src/what.cpp -o exec
---
+++ src/main.c
@@ -0,0 +1,53 @@
+#pragma clang diagnostic ignored "-Weverything"
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#define PORT 43594
+
+int main()
+{
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        printf("Socket creation failed.\n");
+        exit(0);
+    }
+
+    struct sockaddr_in server_addr, client_addr;
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        printf("Bind creation failed.\n");
+        return 0;
+    }
+
+    if (listen(sockfd, 5) == -1) {
+        printf("Socket listening failed.\n");
+        return 0;
+    };
+
+    printf("Server listening on port %d.\n", PORT);
+    socklen_t client_len;
+
+    while (true) {
+        client_len = sizeof(client_addr);
+
+        int new_connfd = accept(sockfd, (struct sockaddr*)&client_addr, &client_len);
+
+        if (new_connfd == -1) {
+            printf("Server accept failed.\n");
+            return 0;
+        }
+
+        printf("Someone connected!\n");
+    }
+
+    return 0;
+}
